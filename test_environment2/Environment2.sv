53bc44829f4ac64fa0b4baab433d43a91dbf545e
diegosalazarsi95 <diegosalazarsi95@gmail.com>
2018-11-20 17:34:17 -0600

    Herencia de clase environment y creación de estimulos

---
+++ test_environment2/Environment2.sv
@@ -0,0 +1,34 @@
+`include "Environment.sv"
+`include "estimulo1.sv"
+
+class environment2 extends environment;
+	estimulo1 est1;
+	estimulo2 est2;
+	estimulo3 est3;
+	estimulo4 est4;
+	
+	function new(virtual intf vif);
+    	super.new(vif);
+    	est1 = new(vif,score,driv,mon);
+    	est2 = new(vif,score,driv,mon);
+    	est3 = new(vif,score,driv,mon);
+    	est4 = new(vif,score,driv,mon);
+  	endfunction
+
+	task run_estimulo1();
+		est1.run();
+	endtask : run_estimulo1
+
+	task run_estimulo2();
+		est2.run();
+	endtask : run_estimulo2
+
+	task run_estimulo3();
+		est3.run();
+	endtask : run_estimulo3
+
+	task run_estimulo4();
+		est4.run();
+	endtask : run_estimulo4
+
+endclass : environment2
---
+++ test_environment2/estimulo4.sv
@@ -0,0 +1,31 @@
+class estimulo4;
+  virtual intf vif;
+  scoreboard    score;
+  driver    	driv;
+  monitor   	mon;
+
+  function new(virtual intf vif, scoreboard score, driver driv, monitor mon);//,scoreboard score);
+    this.vif = vif;
+    this.score = score;
+    this.driv = driv;
+    this.mon = mon;
+  endfunction
+
+  task run();
+    int k;
+    reg [31:0] StartAddr;
+  	for(k=0; k < 20; k++) begin
+      StartAddr = $random & 32'h003FFFFF;
+      driv.burst_write(StartAddr,($random & 8'h0f)+1);  
+      #100;
+      StartAddr = $random & 32'h003FFFFF;
+      driv.burst_write(StartAddr,($random & 8'h0f)+1);  
+      #100;
+      mon.burst_read();  
+      #100;
+      mon.burst_read();  
+      #100;
+    end
+  endtask : run
+
+endclass : estimulo1
