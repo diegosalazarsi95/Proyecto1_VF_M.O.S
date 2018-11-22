3c2ac903c2ec399a501aed8da5fdc4c73d541a89
Gabriel Madrigal <gjmabo@gmail.com>
2018-11-22 14:41:10 -0600

    Agrega el whitebox interface corregido.

--- test_environment2/Whitebox.sv
+++ test_environment2/Whitebox.sv
@@ -30,13 +30,12 @@ interface Whitebox();
 	logic 	[dw-1:0]       WB_app_rd_data        ; // sdr read data
 
 	/****************************************
-	*  These logic has to be implemented using Pads
-	*  **************************************/
+	*  These logic has to be implemented using Pads  *
+	*****************************************/
 	logic 	[SDR_DW-1:0]   WB_pad_sdr_din        ; // SDRA Data Input
 	logic 	[SDR_DW-1:0]   WB_sdr_dout           ; // SDRAM Data Output
 	logic 	[SDR_BW-1:0]   WB_sdr_den_n          ; // SDRAM Data Output enable
 
-	/*  Assigns  */
 	assign WB_app_req 		  = `TOP_PATH.app_req 		 	 ;
 	assign WB_app_req_addr    = `TOP_PATH.app_req_addr	 	 ;
 	assign WB_app_req_len     = `TOP_PATH.app_req_len 		 ;
@@ -54,5 +53,34 @@ interface Whitebox();
 	assign WB_pad_sdr_din     = `TOP_PATH.pad_sdr_din        ;
 	assign WB_sdr_dout        = `TOP_PATH.sdr_dout           ;
 	assign WB_sdr_den_n       = `TOP_PATH.sdr_den_n          ;
+
+	//--------------------------------------------
+	// Assertions Interface signals 
+	//--------------------------------------------
+    logic 					clk 			;
+	logic 					rst 			;
+	logic 					strb 			;
+	logic 					cycle 			;
+	logic 					ack 			;
+	logic 					ras 			;
+	logic 					cas 			; 
+	logic 					cs 				; 
+	logic 					we 				;
+	logic 					sdram_init_done	;
+	logic 	[12:0] 			sdr_addr		;
+	logic 	[SDR_DW-1:0] 	sdr_dq			;
+
+	assign clk				= `TOP_PATH.wb_clk_i;
+	assign rst				= `TOP_PATH.wb_rst_i;
+	assign strb		    	= `TOP_PATH.wb_stb_i;
+	assign cycle			= `TOP_PATH.wb_cyc_i;
+	assign ack	        	= `TOP_PATH.wb_ack_o;
+	assign ras 				= `TOP_PATH.sdr_ras_n;
+	assign cas 				= `TOP_PATH.sdr_cas_n;
+	assign cs 				= `TOP_PATH.sdr_cs_n;
+	assign we 				= `TOP_PATH.sdr_we_n;
+	assign sdram_init_done	= `TOP_PATH.sdr_init_done;
+    assign sdr_addr         = `TOP_PATH.sdr_addr;
+    assign sdr_dq           = `TOP_PATH.sdr_dq;
 
 endinterface
